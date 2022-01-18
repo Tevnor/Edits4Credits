fe6ccf877cf354773229403f0443bfa856243e81
Johannes Rödel <jr125@hdm-stuttgart.de>
2022-01-18 17:05:06 +0100

    Implement move image method with options window for the position. Set the image canvas alignment.

---
+++ src/main/java/org/controller/PositionOptionsController.java
@@ -0,0 +1,63 @@
+package org.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PositionOptionsController implements Initializable {
+
+    @FXML
+    private TextField xPositionInput;
+    @FXML
+    private TextField yPositionInput;
+    @FXML
+    private Button applyPositionChange;
+
+
+    private double xPosition;
+    private double yPosition;
+    private EditorController editorController;
+
+    public void handleApplyPostionChange(ActionEvent event) {
+
+        if (xPositionInput.getText() == ""){
+            xPosition = 0;
+        }
+        else if(xPositionInput.getText() != null){
+            xPosition = Double.parseDouble(xPositionInput.getText());
+        }
+        if (yPositionInput.getText() == ""){
+            yPosition = 0;
+        }
+        else if (yPositionInput.getText() != null){
+            yPosition = Double.parseDouble(yPositionInput.getText());
+        }
+        editorController.setChangedPostion(xPosition, yPosition);
+    }
+
+    public void setEditorController(EditorController editorController) {
+        this.editorController = editorController;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        xPositionInput.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                xPositionInput.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+
+        });
+        yPositionInput.textProperty().addListener((observableValue, oldValue, newValue) -> {
+                    if (!newValue.matches("\\d*")) {
+                        yPositionInput.setText(newValue.replaceAll("[^\\d]", ""));
+                    }
+                }
+        );
+    }
+}
+
